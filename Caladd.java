5c4e62ae4140d551dadd76cb24425b728b3493b8
azema-shaik <skazema221@gmail.com>
2022-08-03 02:46:37 +0530

    test commit 2

--- Caladd.java
+++ Caladd.java
@@ -22,7 +22,7 @@ public static void main(String[] args){
 					break;
 			case '2' : System.exit(0);
 					break;
-			default: System.out.print("INVALID Option Selected !!!");
+			default: System.out.print("INVALID Option Selected, Please select a Vald one !!!!");
 					break;
  
 }
